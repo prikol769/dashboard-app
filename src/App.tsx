e9608bc77f6d02ff9ae56521b1ec694cd0b4efc0
prikol769 <prikol769@gmail.com>
2024-01-24 12:00:17 -0600

    added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Refine, GitHubBanner, WelcomePage } from "@refinedev/core";
+import { Refine, GitHubBanner, Authenticated } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -7,13 +7,15 @@ import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import routerBindings, {
   UnsavedChangesNotifier,
   DocumentTitleHandler,
+  CatchAllNavigate,
 } from "@refinedev/react-router-v6";
 
 import { Home, ForgotPassword, Login, Register } from "./pages";
+import Layout from "./components/layout";
 
 function App() {
   return (
@@ -37,11 +39,23 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
-                <Route index element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                </Route>
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
